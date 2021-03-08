49158d782800a23406003d2477e755f104d21419
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-03-08 15:00:44 +0800

    添加count

--- crawl.py
+++ crawl.py
@@ -9,10 +9,13 @@ url = 'https://dl.acm.org/doi/proceedings/10.1145/3387514'
 browser.get(url)
 # 获取元素
 element_list = browser.find_elements_by_class_name('issue-item__title')
+
+count = 0
 # 拿到标题文字
 for element in element_list:
+    count += 1
     title = element.text
-    print(title)
+    print(str(count) + " : " + title)
 
 # 退出
 browser.quit()
