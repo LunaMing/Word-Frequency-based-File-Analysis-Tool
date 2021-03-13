29beb14c924a59c16b61db7c6af0d0a4f5358431
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-03-13 17:43:11 +0800

    添加停用词表

--- word_freq.py
+++ word_freq.py
@@ -1,3 +1,6 @@
+from nltk.corpus import stopwords
+
+
 def getText():
     txt = open('raw.txt', 'r', encoding='UTF-8').read()
 
@@ -17,3 +20,22 @@ items.sort(key=lambda x: x[1], reverse=True)
 for i in range(10):
     word, count = items[i]
     print("{0:<10}{1:>5}".format(word, count))
+
+# nltk.download('stopwords')
+words = stopwords.words('english')
+print(words)
+
+word_list = []
+for i in range(len(items)):
+    word_list.append(items[i][0])
+
+filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+
+# 打开一个文件
+fo = open("word_result.txt", "w", encoding='UTF-8')
+
+for w in filtered_words:
+    fo.write(str(w) + "\n")
+
+# 关闭打开的文件
+fo.close()
