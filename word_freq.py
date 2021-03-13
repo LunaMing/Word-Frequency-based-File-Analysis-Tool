fc0e63f4e54d467e42648a7033414e4ab6265614
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-03-13 19:34:53 +0800

    将文本中数字替换为空格，停用词表移到前面

--- word_freq.py
+++ word_freq.py
@@ -7,13 +7,24 @@ def getText():
     txt = txt.lower()
     for ch in '!"#$%&()*+,-./:;<=>?@[\\]^_‘{|}~':
         txt = txt.replace(ch, " ")  # 将文本中特殊字符替换为空格
+    for ch in '0123456789':
+        txt = txt.replace(ch, " ")  # 将文本中数字替换为空格
     return txt
 
 
 hamletTxt = getText()
 words = hamletTxt.split()
-counts = {}
+
+# 停用词表
+word_list = []
 for word in words:
+    word_list.append(word)
+
+filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+
+
+counts = {}
+for word in filtered_words:
     counts[word] = counts.get(word, 0) + 1
 items = list(counts.items())
 items.sort(key=lambda x: x[1], reverse=True)
@@ -21,21 +32,11 @@ for i in range(10):
     word, count = items[i]
     print("{0:<10}{1:>5}".format(word, count))
 
-# nltk.download('stopwords')
-words = stopwords.words('english')
-print(words)
-
-word_list = []
-for i in range(len(items)):
-    word_list.append(items[i][0])
-
-filtered_words = [word for word in word_list if word not in stopwords.words('english')]
-
 # 打开一个文件
 fo = open("word_result.txt", "w", encoding='UTF-8')
 
-for w in filtered_words:
-    fo.write(str(w) + "\n")
+for item in items:
+    fo.write(str(item) + "\n")
 
 # 关闭打开的文件
 fo.close()
