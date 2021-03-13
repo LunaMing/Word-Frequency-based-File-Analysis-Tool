181db1d3ef252d280e9f5ef33bc948c34faa0b59
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-03-13 15:38:05 +0800

    网址变更

--- crawl.py
+++ crawl.py
@@ -6,13 +6,13 @@ from selenium.webdriver.common.keys import Keys
 # 打开浏览器 Chrome
 browser = webdriver.Chrome()
 # 准备网址
-url = 'https://dl.acm.org/doi/proceedings/10.1145/3387514'
+url = 'https://www.usenix.org/conference/nsdi20/accepted-papers'
 
 # 访问网址
 browser.get(url)
 
 # 获取元素
-element_list = browser.find_elements_by_class_name('issue-item__title')
+element_list = browser.find_elements_by_class_name('node-title')
 
 # 计数
 count = 0
