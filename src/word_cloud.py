abc7f195114302a3e0e764a0616da7aad47358e6
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-03-14 16:02:14 +0800

    生成词云图片

--- src/word_cloud.py
+++ src/word_cloud.py
@@ -1,6 +1,10 @@
 import wordcloud
+
 from src.word_freq import get_text
-w = wordcloud.WordCloud()
-s = get_text()
-w.generate(s)
-w.to_file('../res/output.png')
+
+
+def get_cloud():
+    w = wordcloud.WordCloud()
+    s = get_text()
+    w.generate(s)
+    w.to_file('../res/output.png')
