6bd053d5ef03ee3fb91185bc1a4e9e7c791c06f8
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-03-14 16:35:55 +0800

    背景改为白色

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ from src.word_cloud import get_cloud
 from src.word_freq import word_deal
 
 # crawl()
-# word_deal()
+word_deal()
 get_cloud()
--- src/word_cloud.py
+++ src/word_cloud.py
@@ -4,7 +4,7 @@ from src.word_freq import get_text
 
 
 def get_cloud():
-    w = wordcloud.WordCloud()
+    w = wordcloud.WordCloud(background_color='white')
     s = get_text()
     w.generate(s)
     w.to_file('../res/output.png')
