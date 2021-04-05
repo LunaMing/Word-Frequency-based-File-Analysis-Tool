192266c016e4f0b6ba15150e0d0259debe3f7123
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-04-05 16:51:53 +0800

    main调用函数

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from src.crawl import crawl
+from src.pdf2text import pdf2text
 from src.word_cloud import get_cloud
 from src.word_freq import word_deal
 
 # crawl()
-word_deal()
-get_cloud()
+# word_deal()
+# get_cloud()
+pdf2text()
