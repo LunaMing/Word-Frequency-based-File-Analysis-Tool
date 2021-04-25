2a35b69ac749f139747135eb0321631d81305628
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-04-25 16:13:46 +0800

    添加注释

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from src.picture import cloud
 from src.word import word_deal
 
+# 爬虫
 # crawl()
 
 pdf_name_list = ["nsdi20spring_arashloo_prepub", "nsdi20spring_birkner_prepub"]
-
 for index in range(len(pdf_name_list)):
     pdf_name = pdf_name_list[index]
+    # 文本处理
     s = word_deal(pdf_name)
+    # 可视化
     cloud(s, pdf_name + ".png")
