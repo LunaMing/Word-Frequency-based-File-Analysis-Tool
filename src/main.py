02e8350a3d63e5c48988a9497905d93f97e64ea6
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-04-25 16:17:22 +0800

    添加第3篇

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.word import word_deal
 # 爬虫
 # crawl()
 
-pdf_name_list = ["nsdi20spring_arashloo_prepub", "nsdi20spring_birkner_prepub"]
+pdf_name_list = ["nsdi20spring_arashloo_prepub", "nsdi20spring_birkner_prepub", "nsdi20spring_burnett_prepub"]
 for index in range(len(pdf_name_list)):
     pdf_name = pdf_name_list[index]
     # 文本处理
