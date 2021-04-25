a1711b876b3baf937feab0f8b2fdbebe43e245c7
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-04-25 16:20:28 +0800

    添加总集图

--- src/main.py
+++ src/main.py
@@ -4,10 +4,13 @@ from src.word import word_deal
 # 爬虫
 # crawl()
 
+total_str = ""
 pdf_name_list = ["nsdi20spring_arashloo_prepub", "nsdi20spring_birkner_prepub", "nsdi20spring_burnett_prepub"]
 for index in range(len(pdf_name_list)):
     pdf_name = pdf_name_list[index]
     # 文本处理
     s = word_deal(pdf_name)
+    total_str += s
     # 可视化
     cloud(s, pdf_name + ".png")
+cloud(total_str, "total.png")
