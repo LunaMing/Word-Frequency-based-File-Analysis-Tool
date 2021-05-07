821b09660ab5244d552444c39b5438080ebe466a
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-07 14:01:33 +0800

    添加neo4j函数

--- src/myneo4j.py
+++ src/myneo4j.py
@@ -1,22 +1,31 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "123456"))
 
-def add_friend(tx, name, friend_name):
-    tx.run("MERGE (a:Person {name: $name}) "
-           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
-           name=name, friend_name=friend_name)
+def add_author_write_paper(tx, name, paper_title):
+    tx.run("MERGE (a:Author {name: $name}) "
+           "MERGE (a)-[:WRITE]->(p:Paper {title: $paper_title})",
+           name=name, paper_title=paper_title)
+
 
 def print_friends(tx, name):
     for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
                          "RETURN friend.name ORDER BY friend.name", name=name):
         print(record["friend.name"])
 
-with driver.session() as session:
-    session.write_transaction(add_friend, "Arthur", "Guinevere")
-    session.write_transaction(add_friend, "Arthur", "Lancelot")
-    session.write_transaction(add_friend, "Arthur", "Merlin")
-    session.read_transaction(print_friends, "Arthur")
 
-driver.close()
-exit()
+def print_author_papers(tx, name):
+    for record in tx.run("MATCH (a:Author)-[:WRITE]->(paper) WHERE a.name = $name "
+                         "RETURN paper.title ORDER BY paper.title", name=name):
+        print(record["paper.title"])
+
+
+def neo4j_driver():
+    driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "123456"))
+
+    with driver.session() as session:
+        session.write_transaction(add_author_write_paper, "author1",
+                                  "title1")
+        session.read_transaction(print_author_papers, "author1")
+
+    driver.close()
+    exit()
