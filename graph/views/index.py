73010eebd08c4545d6eac2f4558d798c02ff617a
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-14 00:38:00 +0800

    从后端尝试传输数据给html

--- graph/views/index.py
+++ graph/views/index.py
@@ -3,11 +3,10 @@ from django.template import loader
 
 
 def getIndex(request):
-    # latest_question_list = []
-    latest_question_list = ["question1", "question2"]
-    context = {'latest_question_list': latest_question_list}
+    latest_question_list = ["author-0", "author-1"]
     template = loader.get_template('graph/index.html')
     context = {
-        'latest_question_list': latest_question_list,
+        'author_0': latest_question_list[0],
+        'author_1': latest_question_list[1],
     }
     return HttpResponse(template.render(context, request))
