163ec6e109ecd6cc4464820516fac16440be2534
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-13 21:26:32 +0800

    查询api

--- graph/urls.py
+++ graph/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from . import views
+from graph.views import *
+from views.index import index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('person', personDetails),
+    path('getAllPersons', getAllPersons),
+    path('city', cityDetails),
+    path('getAllCities', getAllCities),
+    path('connectPaC', connectPaC),
+    path('connectPaP', connectPaP)
 ]
---
+++ graph/views/__init__.py
@@ -0,0 +1,3 @@
+from graph.views.city import *
+from graph.views.connectors import *
+from graph.views.person import *
