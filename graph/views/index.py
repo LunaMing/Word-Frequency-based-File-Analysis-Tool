2117405a3a39d6ab8be12d096c842edc4d71ee44
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-14 01:26:28 +0800

    删除无用单词节点，替换单词

--- graph/views/index.py
+++ graph/views/index.py
@@ -4,17 +4,20 @@ from django.template import loader
 
 def getIndex(request):
     # edges代表节点间的关系数据。
-    edges = '[{source:1,target:0}]'
+    edges = '[{source:1,target:0},{source:1,target:3},{source:1,target:4}]'
+    author_list = ["Mina Tahmasbi Arashloo", "Rüdiger Birkner"]
+    title_list = ['Enabling Programmable Transport Protocols in High-Speed NICs',
+                  'Config2Spec: Mining Network Specifications from Network Configurations']
+    word_list = ['segment', 'tonic', 'flow']
 
-    latest_question_list = ["author-0", "author-1"]
-    title_list = ['Frequency Configuration for Low-Power Wide-Area Networks in a Heartbeat',
-                  'Plankton: Scalable network configuration verification through model checking']
     template = loader.get_template('graph/index.html')
     context = {
-        'author_0': latest_question_list[0],
-        'author_1': latest_question_list[1],
+        'author_0': author_list[0],
+        'author_1': author_list[1],
         'title_0': title_list[0],
         'title_1': title_list[1],
         'edges': edges,
+        'word_0':word_list[0],
+        'word_1': word_list[1],
     }
     return HttpResponse(template.render(context, request))
