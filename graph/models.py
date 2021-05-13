34ec284fab07a9804a59f4e746aec3b3b1ca7ae4
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-13 21:09:19 +0800

    模型和基础连接设置

--- graph/models.py
+++ graph/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from neomodel import StructuredNode, StringProperty, IntegerProperty, UniqueIdProperty, RelationshipTo
 
 
-class Paper(models.Model):
+class PaperInfo(models.Model):
     title = models.CharField(max_length=255)
     author = models.TextField()
     school = models.TextField()
@@ -9,3 +10,18 @@ class Paper(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class City(StructuredNode):
+    code = StringProperty(unique_index=True, required=True)
+    name = StringProperty(index=True, default="city")
+
+
+class Person(StructuredNode):
+    uid = UniqueIdProperty()
+    name = StringProperty(unique_index=True)
+    age = IntegerProperty(index=True, default=0)
+
+    # Relations :
+    city = RelationshipTo(City, 'LIVES_IN')
+    friends = RelationshipTo('Person', 'FRIEND')
