737bacb5e65b7e413ce68dd34e705ac77f6fd1d1
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-14 01:15:27 +0800

    py 传 边

--- graph/views/index.py
+++ graph/views/index.py
@@ -3,6 +3,9 @@ from django.template import loader
 
 
 def getIndex(request):
+    # edges代表节点间的关系数据。
+    edges = '[{source:1,target:0}]'
+
     latest_question_list = ["author-0", "author-1"]
     title_list = ['Frequency Configuration for Low-Power Wide-Area Networks in a Heartbeat',
                   'Plankton: Scalable network configuration verification through model checking']
@@ -12,5 +15,6 @@ def getIndex(request):
         'author_1': latest_question_list[1],
         'title_0': title_list[0],
         'title_1': title_list[1],
+        'edges': edges,
     }
     return HttpResponse(template.render(context, request))
