1a6ddb52a6ccc8dfc720e90d0f2f3bf8572894e0
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-14 23:12:59 +0800

    尝试将edges改回去

--- graph/views/index.py
+++ graph/views/index.py
@@ -3,8 +3,6 @@ from django.template import loader
 
 
 def getIndex(request):
-    # edges代表节点间的关系数据。
-    edges = '[{source:1,target:0},{source:1,target:3},{source:1,target:4}]'
     author_list = ["Mina Tahmasbi Arashloo", "Rüdiger Birkner"]
     title_list = ['Enabling Programmable Transport Protocols in High-Speed NICs',
                   'Config2Spec: Mining Network Specifications from Network Configurations']
@@ -12,7 +10,6 @@ def getIndex(request):
 
     template = loader.get_template('graph/index.html')
     context = {
-        'edges': edges,
         'authors': author_list,
         'papers': title_list,
         'words': word_list,
