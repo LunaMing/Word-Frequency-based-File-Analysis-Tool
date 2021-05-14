03a50fd9e9f4cb2b890d1924df27d87cbba35f5f
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-14 23:29:04 +0800

    左侧使用数组的pdf标题

--- graph/views/index.py
+++ graph/views/index.py
@@ -4,8 +4,10 @@ from django.template import loader
 
 def getIndex(request):
     author_list = ["Mina Tahmasbi Arashloo", "Rüdiger Birkner"]
-    title_list = ['Enabling Programmable Transport Protocols in High-Speed NICs',
-                  'Config2Spec: Mining Network Specifications from Network Configurations']
+    title_list = [
+        'Enabling Programmable Transport Protocols in High-Speed NICs',
+        'Config2Spec: Mining Network Specifications from Network Configurations'
+    ]
     word_list = ['segment', 'tonic', 'flow']
 
     template = loader.get_template('graph/index.html')
