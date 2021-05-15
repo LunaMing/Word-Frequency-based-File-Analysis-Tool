9c3e579f3c45827c80317aeda5e46535e823a4c2
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-15 20:58:48 +0800

    添加转换节点和连接格式函数

--- graph/views/paper.py
+++ graph/views/paper.py
@@ -7,72 +7,28 @@ from graph.models import Paper
 
 
 def getAllPapers(request):
-    pdf_list = []
-    try:
-        papers = Paper.nodes.all()
-        response = []
-        for paper in papers:
-            pdf_list.append(paper.title)
-            word_list = paper.get_contain_words()
-            word_name_list = []
-            for word in word_list:
-                word_id = word.id
-                word_name = word.name
-                word_obj = {
-                    "id": word_id,
-                    "name": word_name
-                }
-                word_name_list.append(word_obj)
-            obj = {
-                "id": paper.id,
-                "title": paper.title,
-                "words": word_name_list
-            }
-            response.append(obj)
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        response = {"error": "Error occurred"}
-        return JsonResponse(response, safe=False)
-
     template = loader.get_template('graph/index.html')
 
-    author_list = ["Mina Tahmasbi Arashloo", "Rüdiger Birkner"]
-    paper_list = [
-        'Enabling Programmable Transport Protocols in High-Speed NICs',
-        'Config2Spec: Mining Network Specifications from Network Configurations'
-    ]
-    word_list = ["machine learning", "flow"]
+    res = get_neo4j_data()
+    pdf_list = ["nsdi20spring_arashloo_prepub.pdf", "nsdi20spring_birkner_prepub.pdf"]
+
     node_list = []
-    node_author = {
+    node_empty = {
         "id": 0,
         "category": 0,
-        "name": author_list[0],
-        "symbol": 'roundRect',
-        "value": 20,
-        "symbolSize": 50
-    }
-    node_list.append(node_author)
-    node_paper = {
-        "id": 1,
-        "category": 1,
-        "name": paper_list[0],
+        "name": '-',
         "symbol": 'rect',
-        "value": 20,
-        "symbolSize": 30
+        "value": 0,
+        "symbolSize": 100
     }
-    node_list.append(node_paper)
-    node_word = {
-        "id": 2,
-        "category": 2,
-        "name": word_list[0],
-        "symbol": 'circle',
-        "value": 20,
-        "symbolSize": 60
-    }
-    node_list.append(node_word)
+    node_list.append(node_empty)
 
-    link_list = [{"source": 0, "target": 1}, {"source": 1, "target": 2}]
+    link_list = []
+
+    data_to_node_and_link(res, node_list, link_list)
+
+    print(link_list)
+    print(node_list)
 
     context = {
         'pdfs': pdf_list,
@@ -80,6 +36,51 @@ def getAllPapers(request):
         'nodes': node_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def data_to_node_and_link(res, node_list, link_list):
+    for paper in res:
+        node_paper = {
+            "id": paper["id"],
+            "category": 1,
+            "name": paper["title"],
+            "symbol": 'rect',
+            "value": 20,
+            "symbolSize": 30
+        }
+        node_list.append(node_paper)
+
+        for author in paper["authors"]:
+            node_author = {
+                "id": author["id"],
+                "category": 0,
+                "name": author["name"],
+                "symbol": 'roundRect',
+                "value": 20,
+                "symbolSize": 50
+            }
+            node_list.append(node_author)
+            link = {
+                "source": author["id"],
+                "target": paper["id"]
+            }
+            link_list.append(link)
+
+        for word in paper["words"]:
+            node_word = {
+                "id": word["id"],
+                "category": 2,
+                "name": word["name"],
+                "symbol": 'circle',
+                "value": 20,
+                "symbolSize": 60
+            }
+            node_list.append(node_word)
+            link = {
+                "source": paper["id"],
+                "target": word["id"]
+            }
+            link_list.append(link)
 
 
 def get_json_data(request):
