7bfdac933d4f752ce99dab5362f7a6d71d0fa154
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-15 22:34:11 +0800

    使用节点index而不是neo4j的index来画图

--- graph/views/graph.py
+++ graph/views/graph.py
@@ -29,52 +29,62 @@ def get_new_graph(request):
 def data_to_node_and_link(res, node_list, link_list):
     word_set = set()
 
+    node_index = 0
+
     for paper in res:
+        paper_id = node_index
         node_paper = {
-            "id": paper["id"],
             "category": 1,
             "name": paper["title"],
             "symbol": 'rect',
-            "value": paper["id"],
+            "value": node_index,
             "symbolSize": 30
         }
         node_list.append(node_paper)
+        node_index += 1
 
         for author in paper["authors"]:
+            author_id = node_index
             node_author = {
-                "id": author["id"],
                 "category": 0,
                 "name": author["name"],
                 "symbol": 'roundRect',
-                "value": author["id"],
+                "value": node_index,
                 "symbolSize": 50
             }
             node_list.append(node_author)
+            node_index += 1
+
             link = {
-                "source": author["id"],
-                "target": paper["id"]
+                "source": author_id,
+                "target": paper_id
             }
             link_list.append(link)
 
         for word in paper["words"]:
+            word_id = -1
             if word["name"] not in word_set:
                 word_set.add(word["name"])
+                word_id = node_index
                 node_word = {
-                    "id": word["id"],
                     "category": 2,
                     "name": word["name"],
                     "symbol": 'circle',
-                    "value": word["id"],
+                    "value": node_index,
                     "symbolSize": 60
                 }
                 node_list.append(node_word)
+                node_index += 1
+            else:
+                # 如果word已经建立了节点，就查找到这个已经建立过的节点id
+                for node in node_list:
+                    if node["name"] == word["name"]:
+                        word_id = node["value"]
             link = {
-                "source": paper["id"],
-                "target": word["id"]
+                "source": paper_id,
+                "target": word_id
             }
             link_list.append(link)
-
-    node_list = sorted(node_list, key=lambda e: e.__getitem__('id'))
 
     print("--node--")
     for node in node_list:
