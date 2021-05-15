06bd6b1cacfd5acdb62dbd012738ddf641754f18
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-15 16:20:22 +0800

    start btn 连接上neo4j的数据库数据

--- graph/views/paper.py
+++ graph/views/paper.py
@@ -1,19 +1,36 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
 
 from graph.models import Paper
 
 
 def getAllPapers(request):
-    if request.method == 'GET':
-        try:
-            papers = Paper.nodes.all()
-            response = []
-            for paper in papers:
-                obj = {
-                    "title": paper.title,
-                }
-                response.append(obj)
-            return JsonResponse(response, safe=False)
-        except:
-            response = {"error": "Error occurred"}
-            return JsonResponse(response, safe=False)
+    try:
+        papers = Paper.nodes.all()
+        response = []
+        for paper in papers:
+            obj = {
+                "title": paper.title,
+            }
+            response.append(obj)
+        # return JsonResponse(response, safe=False)
+    except:
+        response = {"error": "Error occurred"}
+        return JsonResponse(response, safe=False)
+
+    pdf_list = []
+    for paper in papers:
+        pdf_list.append(paper.title)
+
+    author_list = ["author1", "author2"]
+    title_list = pdf_list
+    word_list = ['word1', 'word2', 'word3']
+
+    template = loader.get_template('graph/index.html')
+    context = {
+        'pdfs': pdf_list,
+        'authors': author_list,
+        'papers': title_list,
+        'words': word_list,
+    }
+    return HttpResponse(template.render(context, request))
