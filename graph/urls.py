d4fb9f90e050364d666b8e9a06c213490d4a20f2
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-15 22:58:38 +0800

    找回all paper接口

--- graph/urls.py
+++ graph/urls.py
@@ -5,6 +5,7 @@ from graph.views import *
 urlpatterns = [
     path('', getIndex),
     path('getAllAuthors', getAllAuthors),
+    path('getAllPapers', getAllPapers),
     path('start', get_new_graph),
     path('getAllJson', get_json_data),
 ]
--- graph/views/__init__.py
+++ graph/views/__init__.py
@@ -1,3 +1,4 @@
-from graph.views.graph import *
 from graph.views.author import *
+from graph.views.graph import *
 from graph.views.index import *
+from graph.views.paper import *
