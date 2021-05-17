c269b9898238ca79d53d114b9f38972f47aebe67
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2021-05-17 16:36:13 +0800

    词云最小3字

--- graph/algorithm/cloud.py
+++ graph/algorithm/cloud.py
@@ -2,6 +2,6 @@ import wordcloud
 
 
 def draw_cloud(raw_str: str, cloud_path: str):
-    w = wordcloud.WordCloud(mode="RGBA", background_color=None, include_numbers=True)
+    w = wordcloud.WordCloud(mode="RGBA", background_color=None, min_word_length=3)
     w.generate(raw_str)
     w.to_file(cloud_path)
